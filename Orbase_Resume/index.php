4ebbae809ce5149ef1ced6b157b6b0b4dc3b911a
Jan Hericka Orbase <157817310+Heks123@users.noreply.github.com>
2024-05-30 22:21:17 +0800

    Add files via upload

---
+++ Orbase_Resume/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="resume">
+        <div class="top">
+            <div class="name">JAN HERICKA ORBASE</div>
+            <div class="position">RESUME</div>
+        </div>
+        <div class="down">
+            <div class="left">
+                <div class="photo"><img src="orbase.jpg"></div>
+                <div class="section hello">
+                    <div class="section-title">ABOUT ME</div>
+                    <p>                    
+                       Hi! I'm Jan Hericka Orbase, an experienced full-stack developer with a strong focus on back-end programming and data analysis. 
+                       I have developed several systems, leveraging my expertise in both front-end and back-end technologies. 
+                       I graduated from Bicol University Polangui Campus in 2026, I developed a comprehensive skill set in HTML, CSS, PHP, and SQL. 
+                       Furthermore, I possess proficiency in several programming languages, such as C++, Java, Python, and C#.
+                    </p>
+                </div>
+                <div class="hobby">
+                    <div class="section-title">CONTACT</div>
+                        <ul class="contact-list">
+                            <li class="contact">
+                                <i class="fa fa-phone-square" style="font-size:14px">0967-248-0993</i>
+                            </li>
+                            <li class="contact">
+                                <i class="fa fa-envelope" style="font-size:13.5px"><a href="mailto:janherickaorbase@gmail.com">janherickaorbase@gmail.com</a></i>
+                            </li>
+                            <li class="contact">
+                                <i class="fa fa-instagram" style="font-size:14px"><a href="https://www.instagram.com/jh_orbase?igsh=MnRmN3Z0dWk3bmk3&utm_source=qr">jh_orbase</a></i>
+                            </li>
+                            <li class="contact">
+                                <i class="fa fa-facebook-square" style="font-size:14px"><a href="https://www.facebook.com/erica.orbase/">Jan Hericka Orbase</a></i>
+                            </li>
+                            <li class="contact">
+                                <i class="fa fa-github" style="font-size:14.5px"><a href="https://github.com/Heks123/jhorbase">Heks123/jhorbase</a></i>   
+                            </li>   
+                        </ul>
+                </div>
+            </div>
+            <div class="right">
+                <div class="section work">
+                    <div class="section-title">
+                        PROFESSIONAL EXPERIENCE
+                    </div>
+                    <div class="work-item">
+                        <div class="work-period">2029</div>
+                        <div class="work-position">Full-Stack Web Developer</div>
+                        <div class="work-company">Tech Innovators Inc.</div>
+                        <div class="work-des">Designed and developed responsive and interactive web applications using HTML, CSS, JavaScript, and React.
+                                                Built and maintained RESTful APIs with Node.js and Express to support web applications.
+                                                Collaborated with cross-functional teams to deliver high-quality software products on time and within budget.
+                        </div>
+                    </div>
+                    <div class="work-item">
+                        <div class="work-period">2028</div>
+                        <div class="work-position">Senior Back-End Developer</div>
+                        <div class="work-company">DataWave Solutions.</div>
+                        <div class="work-des">Led the development and optimization of server-side logic, ensuring high performance and responsiveness.
+                                                Managed databases using SQL and NoSQL technologies, including MySQL and MongoDB.
+                                                Implemented robust security measures and performed regular code reviews to maintain code quality and security standards.
+                        </div>
+                    </div>
+                    <div class="work-item">
+                        <div class="work-period">2027</div>
+                        <div class="work-position">Data Analyst and Front-End Developer</div>
+                        <div class="work-company">QuantumWeb Technologies.</div>
+                        <div class="work-des">Analyzed large datasets to derive actionable insights and support data-driven decision-making processes.
+                                                Developed and maintained front-end components using Angular and Vue.js, ensuring seamless user experiences.
+                                                Created data visualization tools and dashboards to present complex data in an accessible and visually appealing manner.
+                        </div>
+                    </div>
+                </div>
+                <div class="section skill">
+                    <div class="section-title">
+                        PROGRAMMING SKILLS
+                    </div>
+                    <ul class="skill-list">
+                        <li class="skill-item">
+                            <span>HTML/CSS</span>
+                            <div class="bar"></div>
+                        </li><li class="skill-item">
+                            <span>PHP</span>
+                            <div class="bar"></div>
+                        </li><li class="skill-item">
+                            <span>JAVA</span>
+                            <div class="bar"></div>
+                        </li><li class="skill-item">
+                            <span>C</span>
+                            <div class="bar"></div>
+                        </li><li class="skill-item">
+                            <span>PHYTON</span>
+                            <div class="bar"></div>
+                        </li><li class="skill-item">
+                            <span>C++</span>
+                            <div class="bar"></div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="section personal">
+                    <div class="section-title">
+                        PERSONAL SKILLS
+                    </div>
+                    <ul class="personal-list">
+                        <li class="personal-item">
+                            <div class="progress">
+                                <div class="space"></div>
+                                <div class="mask">CREATIVITY</div>
+                            </div>
+                        </li><li class="personal-item">
+                            <div class="progress">
+                                <div class="space"></div>
+                                <div class="mask">TEAM WORK</div>
+                            </div>
+                        </li><li class="personal-item">
+                            <div class="progress">
+                                <div class="space"></div>
+                                <div class="mask">INNOVATIVE</div>
+                            </div>
+                        </li><li class="personal-item">
+                            <div class="progress">
+                                <div class="space"></div>
+                                <div class="mask">LOGICAL THINKING</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
