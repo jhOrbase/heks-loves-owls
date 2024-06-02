197811b1de8edcdda7f45fd2419458ff55d54d14
Jan Hericka Orbase <157817310+Heks123@users.noreply.github.com>
2024-06-02 22:59:00 +0800

    updated shipping variables

--- common_user/process_order.php
+++ common_user/process_order.php
@@ -1,6 +1,14 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+    
 <?php
-session_start();
 include_once "../dbconnect.php";
+session_start();
 
 if(isset($_POST['o_payment_method'])){
 
@@ -14,20 +22,14 @@ if(isset($_POST['o_payment_method'])){
     //if payment method is gcash 
     if( $payment_method == "2" ){ 
     ?>
-
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../style/gcash.css">
-</head>
-<body style="background: #c3e6f4">
-
-<form action="process_gcash.php" method="POST" class="payment">
-           <!-- Total Amount to Pay: <b> <?php echo "Php " . number_format($total_amt_to_pay,2); ?> </b> <br>
+      <div class="card p-2">
+       <h3 class="display-3">Input Gcash Payment Details</h3>
+        <form action="process_gcash.php" method="POST">
+           
+           
+           Total Amount to Pay: <b> <?php echo "Php " . number_format($total_amt_to_pay,2); ?> </b> <br>
            Please pay EXACT AMOUNT to this Gcash Account Number: 09672480993
-           <br>Account Name: ADMIN --> 
+           <br>Account Name: ADMIN
            <hr>
                 <input type="text" hidden name="o_total_amt_to_pay" value="<?php echo $total_amt_to_pay; ?>" />
                 <input type="text" hidden name="o_payment_method" value="<?php echo $payment_method; ?>" />
@@ -36,53 +38,37 @@ if(isset($_POST['o_payment_method'])){
                 <input type="text" hidden name="o_shipping_id" value="<?php echo $shipping_id; ?>" />
 
         <!--gcash-->
-
-
-    
-    <body>
-		<div class="panel panel-default credit-card-box">
-			<div class="panel-heading display-table">
-				<div class="row display-tr">
-				    <h3 class="panel-title display-td"> Gcash Payment Details</h3> 
-                </div>
-                <h6 style = "font-size: 12px; padding: 5px 0 0 15px">Please pay EXACT AMOUNT to this Gcash<br> Account Number: 09672480993 <br> Account Name: ADMIN </h6>
-                      
-                    
-			
-			</div>
-		</div>
-		<br>
-		<label for="cardNumber">Gcash Reference Number</label>
-		<input type="text" class="form-control" name="o_gcash_ref_no" placeholder="Reference Number" required autofocus />
-		<span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
-		<label for="cardExpir">Gcash Account Sender Name</label>
-		<input type="text" class="form-control" name="o_gcash_acc_name" placeholder="Sender Name" required />
-		<label for="cardCVC">Gcash Account Number</label>
-		<input type="text" class="form-control" name="o_gcash_acc_no" placeholder="Account Number" required />
-		<label for="couponCode">Gcash Amount Sent</label>
-		<input type="text" class="form-control" name="o_gcash_amt_sent" />
-		<br>
-            <div style="margin-left:30px; font-size: 18px; ">
-                Total Amount to Pay: <br><strong><?php echo "Php " . number_format($total_amt_to_pay,2); ?><strong>
+            <div class="mb-3">
+                <label for="" class="form-label">Gcash Reference Number</label>
+                <input type="text" class="form-control" name="o_gcash_ref_no">
             </div>
-		<button class="blueButton" style="float:right;" type="submit">Confirm Payment</button>
-		<br><br>
-        
-    </form>
-
-
+            <div class="mb-3">
+                <label for="" class="form-label">Gcash Account Sender Name</label>
+                <input type="text" class="form-control" name="o_gcash_acc_name">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Gcash Account Number</labe>
+                <input type="text" class="form-control" name="o_gcash_acc_no">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Gcash Amount Sent</label>
+                <input type="text" class="form-control" name="o_gcash_amt_sent">
+            </div>
+            <input type="submit" value="Save" class="btn btn-primary">
+        </form>
+        </div>
     <?php 
     die();                            
     }    
         
-    $sql_update_order = "UPDATE orders
-                            SET order_phase_status = 2
-                              , payment_method = '$payment_method'
-                              , order_ref_no = '$order_ref_no'
-                              , alternate_address = '$alt_address'
-                              , shipping_id = '$shipping_id'
-                          WHERE user_id = '$user_id' 
-                            AND order_phase_status = '1';";
+    $sql_update_order = "UPDATE `orders`
+                            SET `order_phase_status` = 2
+                              , `payment_method` = '$payment_method'
+                              , `order_ref_no` = '$order_ref_no'
+                              , `alternate_address` = '$alt_address'
+                              , `shipping_id` = '$shipping_id'
+                          WHERE `user_id` = '$user_id' 
+                            AND `order_phase_status` = '1';";
                     
     $execute_update_order = mysqli_query($conn, $sql_update_order);
     
@@ -95,6 +81,5 @@ if(isset($_POST['o_payment_method'])){
 }
 ?>
 <script src="../js/bootstrap.js"></script>
-<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 </body>
 </html>
