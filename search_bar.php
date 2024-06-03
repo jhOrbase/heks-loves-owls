4b76cbf0cfef0f7b41436b859ea9640122786a92
Jan Hericka Orbase <157817310+Heks123@users.noreply.github.com>
2024-06-03 12:23:04 +0800

    Update search_bar.php

--- search_bar.php
+++ search_bar.php
@@ -6,18 +6,24 @@ session_start();
 if(isset($_SESSION['user_id'])){
    $user_id = $_SESSION['user_id'];
 }
-else{
-   $user_id = '';
+
+if($_SESSION['users_user_type'] != 'C'){
+    header("location:index.php");
+    exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>search page</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/homepage.css">   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"> 
+    <link rel="stylesheet" href="https://unpkg.com/ionicons@5.5.2/dist/css/ionicons.min.css">
+    <title>search page</title>
    
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -28,6 +34,23 @@ else{
 </head>
 <body>
 
+<div class="top-border">
+        <div class="dropdown">
+            <button class="dropdown-button"><ion-icon name="menu-outline"></ion-icon></button> <!-- Changed icon name to include -outline -->
+            <div class="dropdown-content">
+                <a href="common_user/index.php?page=home" class="btn btn-link" style="text-decoration: none; color: white; font-family: 'Montserrat';">
+                    <ion-icon name="home-outline"></ion-icon> Home
+                </a>
+                <a href="common_user/index.php?page=cart" class="btn btn-link" style="text-decoration: none; color: white; font-family: 'Montserrat';">
+                    <ion-icon name="cart-outline"></ion-icon> Cart
+                </a>
+                <a href="common_user/index.php?page=myorder" class="btn btn-link" style="text-decoration: none; color: white; font-family: 'Montserrat';">
+                    <ion-icon name="bag-outline"></ion-icon> My Order
+                </a>
+            </div>
+        </div>
+</div>
+
 <section class="search-form">
     <form action="" method="POST"><!--html skanya? -->
         <input type="search" name="search" placeholder="Find your wristique" required>
@@ -36,7 +59,6 @@ else{
 </section>
 
    <div class="box-container">
-
     <?php
     if (isset($_POST['submit'])) {
       $search = $_POST['search'];
@@ -50,23 +72,27 @@ else{
     if($count_result>0){
     while($row=mysqli_fetch_assoc($execute_search)){
     ?>
-    <form action="" method="post" class="box">
-       <input type="hidden" name="pid" value="<?= htmlspecialchars($row['pdt_id']); ?>">
-       <input type="hidden" name="name" value="<?= htmlspecialchars($row['pdt_name']); ?>">
-       <input type="hidden" name="price" value="<?= htmlspecialchars($row['pdt_price']); ?>">
-       <input type="hidden" name="image" value="<?= htmlspecialchars($row['pdt_img']); ?>">
- 
-       
-       <!-- <a href="quick_view.php?pid=<?= htmlspecialchars($row['pdt_id']); ?>" class="fas fa-eye"></a> -->
-       <img src="webpics/<?= htmlspecialchars($row['pdt_img']); ?>" alt="">
-       <div class="name"><?= htmlspecialchars($row['pdt_name']); ?></div>
-       <div class="flex">
+    <div class="row">
+        <div class="container">
+            <form action="" method="post" class="box">
+            <input type="hidden" name="pid" value="<?= htmlspecialchars($row['pdt_id']); ?>">
+            <input type="hidden" name="name" value="<?= htmlspecialchars($row['pdt_name']); ?>">
+            <input type="hidden" name="price" value="<?= htmlspecialchars($row['pdt_price']); ?>">
+            <input type="hidden" name="image" value="<?= htmlspecialchars($row['pdt_img']); ?>">
+        
+            
+            <!-- <a href="quick_view.php?pid=<?= htmlspecialchars($row['pdt_id']); ?>" class="fas fa-eye"></a> -->
+            <img src="webpics/<?= htmlspecialchars($row['pdt_img']); ?>" alt="" style="width: 150px;">
+            <div class="name"><?= htmlspecialchars($row['pdt_name']); ?></div>
+            <div class="flex">
 
-          <div class="price"><span>$</span><?= htmlspecialchars($row['pdt_price']); ?><span>/-</span></div>
-          <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
-       </div>
-       <input type="submit" value="add to cart" class="btn" name="add_to_cart">
-    </form>
+                <div class="price"><span>$</span><?= htmlspecialchars($row['pdt_price']); ?><span>/-</span></div>
+                <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
+            </div>
+            <a href="common_user/index.php?page=cart&checkout" class="btn">Add to cart</a>
+            </form>
+        </div>
+    </div>
     <?php
               }
           } else {
@@ -77,8 +103,9 @@ else{
  </div>
 <?php } ?>
  
- <script src="js/script.js"></script>
- 
  </body>
+    <script src="js/script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
  </html>
  
